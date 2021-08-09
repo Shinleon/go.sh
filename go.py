cc578be4651f056d3df4ca5322b479f2c6257e8d
Leonard Shin <leonardshin@Leonards-Air.hsd1.wa.comcast.net>
2021-08-08 20:13:42 -0700

    inital commit

---
+++ go.py
@@ -0,0 +1,87 @@
+import sys
+import os
+import csv
+
+def init():
+    csv_location = 'bin/go_data/directory_alias.csv'
+    return os.path.join(os.path.expanduser('~'), csv_location)
+
+def in_csv(f_dir, alias):
+    with open(f_dir, 'r') as inp:
+        for row in csv.reader(inp):
+            if row[0].strip() == alias:
+                return row[1]
+    return None
+
+def add_to_csv(f_dir, alias, out_dir=os.getcwd()):
+    if alias == "alias":
+        return "cannot add an alias named \"alias\""
+    temp_dir = in_csv(f_dir, alias)
+    if type(temp_dir) == str:
+        return f"Cannot add duplicate alias: \"{alias}\" tied to \"{temp_dir}\""
+    with open(f_dir, 'a', newline='') as out:
+        csv_writer = csv.writer(out, delimiter=',',
+                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([alias, out_dir])
+    return f"Added alias \"{alias}\" tied to \"{out_dir}\""
+
+def rm_frm_csv(f_dir, alias):
+    if alias == "alias":
+        return "cannot remove an alias named \"alias\""
+    pairs = list()
+    directory = None
+    with open(f_dir, 'r') as inp:
+        for row in csv.reader(inp):
+            if row[0].strip() != alias:
+                pairs.append(row)
+            else:
+                directory = row[1]
+    with open(f_dir, 'w') as out:
+        dir_writer = csv.writer(out, delimiter=',',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for pair in pairs:
+            dir_writer.writerow(pair)
+    if directory == None:
+        return f"Couldn't find alias of the name: \"{alias}\""
+    return f"Removes alias \"{alias}\" tied to  \"{directory}\""
+
+def read_directories(f_dir):
+    ret = ""
+    with open(f_dir, 'r') as inp:
+        csv_reader = csv.reader(inp)
+        for row in csv_reader:
+            ret += str(row)
+        ret += "   "
+    return ret
+
+
+
+def main():
+    parameters = sys.argv[1:]
+
+    if len(parameters) == 0:
+        all_data = read_directories(storage_dir)
+        print(all_data)
+    elif len(parameters) == 1:
+        #print("We should move to", parameters[0])
+        directory = in_csv(storage_dir, parameters[0])
+        print(directory)
+    else:
+        #print("We're either adding or removing an alias.")
+        if parameters[0] == "rm":
+            msg = rm_frm_csv(storage_dir, parameters[1])
+            print(msg)
+        elif parameters[0] == "add":
+            msg = add_to_csv(storage_dir, parameters[1])
+            print(msg)
+    
+
+
+if __name__ == "__main__":
+    storage_dir = init()
+    if not os.path.exists(storage_dir):
+        with open(storage_dir, 'w', newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    main()
+    sys.exit(0)
---
+++ go.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# run as source or this can't move directories but mess up
+
+# echo number of parameters $#
+# for argument in $@
+# do
+#     echo $argument 
+# done
+
+PY_GO="/Users/leonardshin/bin/go.py"
+if [ $# -gt 2 ]
+then
+    echo "This script doesn't need more than two parameters"
+elif [ $# -eq 2 ]
+then
+    # echo "There's a command and an alias"
+    # echo "assuming current directory is the directory"
+    a=`python3 $PY_GO $1 $2`
+    echo $a
+elif [ $# -eq 1 ]
+then
+    #echo "We're gonna try to move to the directory"
+    if [ -d $1 ]
+    then
+        cd $1
+        ls -liFAG
+    else
+        #echo "\"$1\" doesn't exist as a directory"
+        #echo "going to check aliases"
+        a=`python3 $PY_GO $1`
+        # echo $a
+        if [ -d $a ]
+        then 
+            cd $a
+            ls -liFAG
+        else
+            echo "No such directory or alias \"$1\""
+        fi
+    fi
+else
+    a=`python3 $PY_GO`
+    echo
+    echo "The command \`go\` is meant to help with navigation. It's purpose is for you"
+    echo "to store commonly used directories as aliases and be able to easily navigate"
+    echo "to them by typing \"go <alias>\". Of course you will need to be able to add"
+    echo "and remove alias directories so there are two variations that come with \`go\`"
+    echo "In order to get full results please run by prepending \`source\` so that the"
+    echo "script is capable of changing directories"
+    echo
+    echo "The possible entry formats are"
+    echo "1:: go add <alias>  # which assumes the current directory is the one to alias as"
+    echo "2:: go rm  <alias>"
+    echo "3:: go <directory or alias>"
+    echo "4:: go # which will print out this help message"
+    echo
+    echo "list of [current aliases, directories]"
+    echo $a
+    
+fi
